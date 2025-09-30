db27d85db2098b9ab4b3700ef30f0448fd541615
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-09-30 09:26:37 +0200

    JSDOCS

---
+++ UT1/ACT_0.1_jsdoc/test.js
@@ -0,0 +1,11 @@
+
+/**
+ * @summary Suma
+ * @description Metodo de suma
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function suma(a, b) {
+    return a + b
+}
---
+++ UT1/ACT_1/js/ej2.js
@@ -0,0 +1,5 @@
+function helloWorld() {
+    alert('Hola mundo JS')
+}
+
+helloWorld()
